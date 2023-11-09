b6e616b9c469962265141aaffe27a37bdf8fac9f
marooned22 <juansebas2298@hotmail.com>
2023-11-09 09:41:35 -0500

    entrega final

---
+++ main.js
@@ -0,0 +1,12 @@
+var elementoClicable = document.getElementById("elementoClicable");
+var input = elementoClicable.querySelector("input");
+
+document.addEventListener("click", function(event) {
+    if (event.target !== input && event.target !== elementoClicable) {
+        elementoClicable.classList.remove("clickeado");
+    }
+});
+
+elementoClicable.addEventListener("click", function() {
+    elementoClicable.classList.toggle("clickeado");
+});
